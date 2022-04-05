f5ca2e58e0956be969c0479da9c923662e856e32
Diego A. Suaza R <diegos9307@gmail.com>
2022-04-05 00:58:32 -0500

    Funciona pero hay que mejorar el codigo, filtrando con un input

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
-import React, { Component } from 'react';
-import posts from './posts'
-
+import React, { useState } from "react";
+import posts from "./posts";
 
 // Modifica el componente App para implmentar la funcionalidad requerida
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
-        </div>
-        <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
-        </ul>
+const App = () => {
+  const [filter, setFilter] = useState("");
+  const [data] = useState(posts);
+
+  // const handleFilter = (e) => {
+  //   setFilter(e.target.value);
+  //   filtrar(e.target.value);
+  // };
+
+  // const filtrar = (terminoBusqueda) => {
+  //   var resultadosBusqueda = data.filter((el) => {
+  //     if (
+  //       el.title
+  //         .toString()
+  //         .toLocaleLowerCase()
+  //         .includes(terminoBusqueda.toLocaleLowerCase())
+  //     ) {
+  //       return el;
+  //     }
+  //   });
+  //   setData(resultadosBusqueda);
+  // };
+
+  return (
+    <div>
+      <div className="filter">
+        <input
+          type="text"
+          placeholder="Ingresa el término de búsqueda"
+          value={filter}
+          // onChange={(handleFilter)}
+          onChange={(e) => {
+            setFilter(e.target.value);
+          }}
+        />
       </div>
-    )
-  }
-}
+      <ul>
+        {data
+          .filter((val) => {
+            if (filter === "") {
+              return val;
+            } else if (
+              val.title.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+            ) {
+              return val;
+            }
+          })
+          .map(({ url, image, title }, index) => (
+            <li key={index}>
+              <a href={url}>
+                <img src={image} alt="" />
+              </a>
+              <p>{title}</p>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
 
-
-export default App
-
-
+export default App;
